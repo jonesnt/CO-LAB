2f67d7f19a2add9b36992dca1828775428b0bb6f
aidanhadley <asahadleya2@gmail.com>
2023-10-12 14:06:30 -0400

    Complete #133

---
+++ Customizer.java
@@ -0,0 +1,17 @@
+import java.awt.*;  //  this might need to be changed down the road
+
+public class Customizer {
+  private Color bgColor;
+
+  public Customizer() {
+    // TODO
+  }
+
+  public void changeBG(Color color) {
+    // TODO
+  }
+  
+  public void changeTaskColor(Color color, Task task) {
+    // TODO
+  }
+}
