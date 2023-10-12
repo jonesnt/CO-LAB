a56d60805e440647e0d715a1d5f7a14f7ac27947
aidanhadley <asahadleya2@gmail.com>
2023-10-12 14:00:21 -0400

    Complete #134

---
+++ ToDo.java
@@ -0,0 +1,22 @@
+
+public class ToDo {
+  private String task;
+  private boolean completion;
+  private String toDoID;
+
+  public ToDo(String toDoName, int priority, String demarkation) {
+    // TODO
+  }
+
+  public void changeCompletion() {
+    // TODO
+  }
+
+  public void editToDo(String newToDo) {
+    // TODO
+  }
+
+  public boolean changePriority(int priority) {
+    // TODO
+  }
+}
