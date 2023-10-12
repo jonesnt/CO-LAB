f1eb5ed2032201ec0db2d930e8456271b0e472ca
aidanhadley <asahadleya2@gmail.com>
2023-10-12 14:21:13 -0400

    Complete #117

--- ToDo.java
+++ ToDo.java
@@ -3,9 +3,13 @@ public class ToDo {
   private String task;
   private boolean completion;
   private String toDoID;
+  // NOT from UML, but needed.
+  private String toDoName;
+  private int priority;
+  private String demarkation;
 
   public ToDo(String toDoName, int priority, String demarkation) {
-    // TODO
+    
   }
 
   public void changeCompletion() {
@@ -13,7 +17,7 @@ public class ToDo {
   }
 
   public void editToDo(String newToDo) {
-    // TODO
+    this.task = newToDo;
   }
 
   public boolean changePriority(int priority) {
