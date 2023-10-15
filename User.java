5d25e4d65d2485b855171daae479a1fa0422efbc
jonesnt <98778766+jonesnt@users.noreply.github.com>
2023-10-14 23:15:41 -0400

    Closed issue #140. Created User Stub

---
+++ User.java
@@ -0,0 +1,49 @@
+import java.util.UUID;
+
+/**
+ * @author jonesnt
+ */
+public class User {
+    private UUID userID;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public User(String username, String firstName, String lastName,
+    String password) {
+        userID = UUID.randomUUID();
+        setUsername(username);
+        setFirstName(firstName);
+        setLastName(lastName);
+        setPassword(password);
+        // TODO find out if there is a way to reject instantiation
+        // in the even that an improper user/password is given
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public boolean setUsername(String username) {
+        // TODO MUST CHECK DATABASE AGAINST TAKEN USERNAMES
+        return false;
+    }
+
+    public void setFirstName(String firstName) {
+        firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        lastName = lastName;
+    }
+
+    public boolean setPassword(String password) {
+        // TODO check against password requirements
+        return false;
+    }
+}
