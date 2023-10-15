238e12756f812e36e7adc70d5a17e9f7c5b56e46
Cyber-Pyrate <dillionnorris2003@gmail.com>
2023-10-15 15:30:11 -0400

    added #138

---
+++ ProjectManager.java
@@ -0,0 +1,35 @@
+
+/*
+ * by 
+ *  Dillion Norris
+ */
+
+import java.util.ArrayList;
+
+public class ProjectManager {
+    
+    private ArrayList<Project> projects = new ArrayList<Project>();
+    private Project currrentProject;
+
+    ProjectManager projectManager(){};
+
+    //gets instance of ProjectManager
+    public ProjectManager getInstance(){
+    } 
+    //changes aspects of a project 
+    // may need to edit peramiters
+    public boolean changeproject(int projectNum){
+    }
+    //adds a new project
+    // may need to edit peramiters
+    public boolean addproject(String projectName){
+    }
+    //removes a project
+    // may need to edit peramiters
+    public boolean removeProject(String projectName){
+    }
+    //edits a project
+    // may need to edit peramiters
+    public boolean editproject(String projectName){
+    }
+}
