f288ef792e6d8d13647acb02d113535a05484db3
Cyber-Pyrate <dillionnorris2003@gmail.com>
2023-10-15 13:58:23 -0400

    added #52

--- Project.java
+++ Project.java
@@ -1,48 +1,79 @@
 
-Project Project();
-private String projectID;
-private String name;
-private String description;
-private Date datetime;
-private ArrayList<User> assignedUsers = new ArrayList<User>();
-private ArrayList<Task> tasks = new ArrayList<Task>();
-private ArrayList<String> demarkationList = new ArrayList<String>();
-private HashMap<String,ArrayList<Task>> projectElements = new HashMap<String,ArrayList<Task>>();
+/* 
+ * by   
+ *   Dillion Norris
+ */
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public Task createTask(String name,int priority,String demarkation){
-    return Task;
-}
-public Task assignTask(User user){
-    return User;
-}
-public Boolean changeTaskPriority(Task taskName,int priority){
-    return Boolean;
-}
-public void removeTask(Task taskName){
+public class Project {
+
+    Project project = new Project();
+
+    private String projectID;
+    private String name;
+    private String description;
+    private Date datetime;
+    private ArrayList<User> assignedUsers = new ArrayList<User>();
+    private ArrayList<Task> tasks = new ArrayList<Task>();
+    private ArrayList<String> demarkationList = new ArrayList<String>();
+    private HashMap<String, ArrayList<Task>> projectElements = new HashMap<String, ArrayList<Task>>();
+
+    public Task createTask(String name, int priority, String demarkation) {
+
+        //creates a new task
+        Task task = new Task();
+        // assigns name
+        name = name;
+        // assigns priority
+        priority = priority;
+        // assigns demarkation
+        demarkation = demarkation;    // may need to be fixed/clarification
+        // returns task
+        return task;
+    }
+
+    public Task assignTask(Task task ,User user) {
+        task.assignedUsers.add(user);
+        return task;
+    }
+
+    public Boolean changeTaskPriority(Task taskName, int priority) {
+        return Boolean;
+    }
+
+    public void removeTask(Task taskName) {
+
+    }
+
+    public Boolean removeUser(User user, Task taskName) {
+        return Boolean;
+    }
+
+    public Boolean addProject(String name, String description) {
+        return Boolean;
+    }
+
+    public Boolean removeProject(Project project) {
+        return Boolean;
+    }
+
+    public Boolean adUser(User user) {
+        return Boolean;
+    }
+
+    public String printFaq() {
+        return String;
+    }
+
+    public Boolean addDemarkation(String demarkation) {
+        return Boolean;
+    }
+
+    public Boolean removeDemarkation(String demarkation) {
+        return Boolean;
+    }
 
 }
-public Boolean removeUser (User user, Task taskName){
-    return Boolean;
-}
-public Boolean addProject(String name, String description){
-    return Boolean;
-}
-public Boolean removeProject(Project project){
-    return Boolean;
-}
-public Boolean adUser(User user){
-    return Boolean;
-}
-public String printFaq(){
-return String;
-}
-public Boolean addDemarkation(String demarkation){
-return Boolean;
-}
-public Boolean removeDemarkation(String demarkation){
-    return Boolean;
-}
-
-
-
