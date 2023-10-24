018582b206646a3cfd72d9e3e61a39cf9d59c957
jonesnt <98778766+jonesnt@users.noreply.github.com>
2023-10-24 14:17:16 -0400

    Added new methods to ProjectManager

--- ProjectManager.java
+++ ProjectManager.java
@@ -8,29 +8,55 @@ import java.util.ArrayList;
 
 public class ProjectManager {
     
-    private ArrayList<Project> projects = new ArrayList<Project>();
+    private static ProjectManager instance;
+    private ArrayList<Project> projects = null;
     private Project currrentProject;
+    private Task currentTask;
+    private ToDo currentToDo;
 
-    ProjectManager projectManager(){};
+    private ProjectManager(){
+        projects = new ArrayList<Project>();
+        
+    };
 
     //gets instance of ProjectManager
     public static ProjectManager getInstance(){
-    } 
+        if(instance == null)
+            instance = new ProjectManager();
+        return instance;
+    }
+
     //changes aspects of a project 
     // may need to edit peramiters
     public boolean changeproject(int projectNum){
+
     }
     //adds a new project
     // may need to edit peramiters
     public boolean addproject(String projectName){
+        if(checkNames(projectName)) {
+            projects.add(new Project(projectName));
+            return true;
+        }
+        return false;
     }
+
     //removes a project
     // may need to edit peramiters
     public boolean removeProject(String projectName){
+        for(Project specificProject : projects) {
+            if(specificProject.equals(projectName)) {
+                projects.remove(specificProject);
+                return true;
+            }
+        }
+        return false;
     }
+    
     //edits a project
     // may need to edit peramiters
     public boolean editproject(String projectName){
+
     }
     
     public Project getCurrentProject() {
@@ -42,6 +68,18 @@ public class ProjectManager {
     }
 
     public ToDo getCurrentToDo() {
-        return currentToDo
+        return currentToDo;
+    }
+
+    public ArrayList<Comment> getComments() {
+        return currentTask.getComments();
+    }
+
+    private boolean checkNames(String name) {
+        for(Project specificProject : projects) {
+            if(specificProject.equals(name))
+                return false;
+        }
+        return true;
     }
 }
