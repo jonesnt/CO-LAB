782b984c405080314151899aa3becf3052f4a0e7
Cyber-Pyrate <dillionnorris2003@gmail.com>
2023-11-02 02:28:57 -0400

    ui

--- TUI.java
+++ TUI.java
@@ -1,49 +1,59 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.UUID;
 
 public class TUI {
   public static void main(String[] args) {
     Facade f = Facade.getInstance();
+    Scanner in = new Scanner(System.in);
+    Boolean login =false;
+    while (!login){
+    System.out.println("-----------------------------------------------------------------");
+    System.out.println(" ");
+    System.out.println("Username:");//aMadden
+    String username = in.nextLine();
+    System.out.println("Password:");//1234
+    String pass= in.nextLine();
+
+    login = f.logInUser(username, pass);
+    if(!login){
+      System.out.println("login Unsuccessful");
+    System.out.println(" ");
+    System.out.println("----------------------------------------------------------------");
+    break;
+    }
+     if (login){
+      System.out.println("login Successfull");
+     }
+
+  }
+ 
+
+    ArrayList<Project> projs = f.getProjectList();
+    System.out.println("chose project:");
+    for (int i = 1; i < projs.size(); i++)
+    System.out.println(projs.get(i).getName() + "\n");
+
+
+    /* 
+    Facade f = Facade.getInstance();
     boolean yes = f.logInUser("sMcNug", "password");
     System.out.println(yes);
     System.out.println(f.getCurrentUser().getFirstName());
-    Project projectepic = new Project("gay", "the gayest");
-    f.addProject(projectepic);
     ArrayList<Project> projs = f.getProjectList();
     f.changeCurrentProject(1);
     System.out.println(f.getCurrentProject().getName());
-    Task beep = new Task("ouch", "wowzers", f.getCurrentProject().getColumnList().get(1), f.getCurrentUser());
-    yes = f.addTask(beep);
-    System.out.println(yes);
-    ArrayList<Task> tasks = f.getTaskList();
-    for (Task task : tasks) {
-      System.out.println(task.getName());
-    }
-    f.changeCurrentTask(1);
-    Comment comment = new Comment("BLOWS UP", f.getCurrentUser().getUserID(), null);
-    yes = f.addComment(comment);
-    ArrayList<Comment> comments = f.getCommentList();
-    for (Comment comment1 : comments) {
-      System.out.println(comment1.getCommentData());
-    }
-    yes = f.changeCurrentComment(2);
-    System.out.println(yes);
-    System.out.println(f.getCurrentComment().getCommentData());
-    Comment epic = new Comment("epic", f.getCurrentUser().getUserID(), f.getCurrentComment());
-    yes = f.addComment(epic);
-    System.out.println(yes);
-    yes = f.changeCurrentComment(1);
-    System.out.println(yes);
-    System.out.println(f.getCurrentComment().getCommentData());
-    yes = f.changeCurrentComment(0);
-    System.out.println(yes);
-    System.out.println(f.getCurrentComment().getCommentData());
-    f.logOutUser();
-    // for (Project p : f.getMasterList()) {
-    //   System.out.println(p.getName());
-    // }
-    // f.changeCurrentProject(1);
-    // f.changeCurrentComment(2);
 
+    Project project = new Project(null, "project","hell", null, null, null, null);
+    projs.add(project);
+    
+  
+    f.addProject(project);
+
+   
+    
+    System.out.println(f.getProjectList().get(1).getName());
+*/
   }
 }
+
