4c351ee408fa7a5afd732cfd8fce2d05d544ea7a
aidanhadley <asahadleya2@gmail.com>
2023-12-05 14:11:47 -0500

    Work on more DataReader/Writer tests

--- DataReaderTest.java
+++ DataReaderTest.java
@@ -47,7 +47,7 @@ public class DataReaderTest {
       project.addColumn("testing");
       Task toAdd = new Task("test for " + project.getName(), "test task", "testing", users.get(0));
       ToDo todo = new ToDo("ToDo Test");
-      //unable to test comments because we are not able to make a new comment
+      //TODO: test columns and test comments
       todo.assignUser(users.get(0).getUserID());
       toAdd.addToDo(todo, users.get(0));
       //  DEBUG
@@ -57,8 +57,24 @@ public class DataReaderTest {
       tasks.add(toAdd);
 
     }
-    //  DEBUG
-    // System.out.println("MIDDLE");
+    //  test columns
+    Project p = projects.get(0);
+    p.addColumn("second column");
+    Task add = new Task("Second Column", " ", "second column", users.get(0));
+    p.addTask(add, "second column");
+    Task add2 = new Task("column 1 task 2", " ", "testing", users.get(0));
+    Task add3 = new Task("2nd col 2nd task", "", "second column", users.get(0));
+    Task add4 = new Task("2nd col 3rd task", " ", "second column", users.get(0));
+    p.addTask(add2, "testing");
+    p.addTask(add3, "second column");
+    p.addTask(add4, "second column");
+    /*
+    laid out like this:
+    testing | second columns
+    test for| second column
+    col1 t2 | 2nd col 2nd task
+            | 2nd col 3rd task
+     */
     DataWriter.saveProjects(projects);
     DataWriter.saveTasks(tasks);
     // DEBUG
@@ -101,6 +117,36 @@ public class DataReaderTest {
     ArrayList<Task> tasksReceived = DataReader.getTasks();
     for (int i = 0; i < tasks.size(); i++){
       assertEquals(tasksReceived.get(i).getID(), tasks.get(i).getID());
+    }
+  }
+
+  @Test
+  void testColumns() {
+    ArrayList<Project> ps = DataReader.getProjects();
+    ArrayList<String> trueCols = new ArrayList<>();
+    trueCols.add("testing");
+    trueCols.add("second column");
+    Project p = ps.get(0);
+    ArrayList<String> cs = p.getColumnList();
+    for (int i = 0; i < cs.size(); ++i) {
+      assertEquals(cs.get(i), trueCols.get(i));
+    }
+  }
+
+  @Test
+  void testTaskColumns() {
+    /*
+    laid out like this:
+    testing | second columns
+    test for| second column
+    col1 t2 | 2nd col 2nd task
+            | 2nd col 3rd task
+    */
+    ArrayList<Project> ps = DataReader.getProjects();
+    ArrayList<Task> trueTs = projects.get(0).getTasks();
+    ArrayList<Task> rTs = ps.get(0).getTasks();
+    for (int i = 0; i < rTs.size(); ++i) {
+      assertEquals(trueTs.get(i).getID(), rTs.get(i).getID());
     }
   }
 
