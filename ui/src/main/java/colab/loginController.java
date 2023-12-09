e5bd2b4bfbfd3e02275bb8104a17e7a7cd7237f7
aidanhadley <asahadleya2@gmail.com>
2023-12-09 00:05:34 -0500

    Final Fixes

--- ui/src/main/java/colab/loginController.java
+++ ui/src/main/java/colab/loginController.java
@@ -32,8 +32,8 @@ public class loginController implements Initializable {
         // validate login
         Manager m = Manager.getInstance();
 
-        // if (m.logInUser(username, password)) {
-        if (m.logInUser("aMadden", "1234")) {
+        if (m.logInUser(username, password)) {
+        // if (m.logInUser, "1234")) {
             // login successful
             // set rooot home
             App.setRoot("home");
